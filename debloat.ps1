1dfa5965d3d7215e1295fab6c441fb2f99ec0df0
pertrush1 <52548225+pertrush1@users.noreply.github.com>
2021-05-20 12:48:12 +0200

    Update debloat.ps1
    Added Scoop

--- debloat.ps1
+++ debloat.ps1
@@ -1484,4 +1484,187 @@ Remove-Item -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\E
 #echo "Disabling tile push notification"
 #New-FolderForced -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications"
 #sp "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" "NoTileApplicationNotification" 1
+#Requires -Version 5
+
+# remote install:
+#   Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+$old_erroractionpreference = $erroractionpreference
+$erroractionpreference = 'stop' # quit if anything goes wrong
+
+if (($PSVersionTable.PSVersion.Major) -lt 5) {
+    Write-Output "PowerShell 5 or later is required to run Scoop."
+    Write-Output "Upgrade PowerShell: https://docs.microsoft.com/en-us/powershell/scripting/setup/installing-windows-powershell"
+    break
+}
+
+# show notification to change execution policy:
+$allowedExecutionPolicy = @('Unrestricted', 'RemoteSigned', 'ByPass')
+if ((Get-ExecutionPolicy).ToString() -notin $allowedExecutionPolicy) {
+    Write-Output "PowerShell requires an execution policy in [$($allowedExecutionPolicy -join ", ")] to run Scoop."
+    Write-Output "For example, to set the execution policy to 'RemoteSigned' please run :"
+    Write-Output "'Set-ExecutionPolicy RemoteSigned -scope CurrentUser'"
+    break
+}
+
+if ([System.Enum]::GetNames([System.Net.SecurityProtocolType]) -notcontains 'Tls12') {
+    Write-Output "Scoop requires at least .NET Framework 4.5"
+    Write-Output "Please download and install it first:"
+    Write-Output "https://www.microsoft.com/net/download"
+    break
+}
+
+# get core functions
+$core_url = 'https://raw.githubusercontent.com/lukesampson/scoop/master/lib/core.ps1'
+Write-Output 'Initializing...'
+Invoke-Expression (new-object net.webclient).downloadstring($core_url)
+
+# prep
+if (installed 'scoop') {
+    write-host "Scoop is already installed. Run 'scoop update' to get the latest version." -f red
+    # don't abort if invoked with iex that would close the PS session
+    if ($myinvocation.mycommand.commandtype -eq 'Script') { return } else { exit 1 }
+}
+$dir = ensure (versiondir 'scoop' 'current')
+
+# download scoop zip
+$zipurl = 'https://github.com/lukesampson/scoop/archive/master.zip'
+$zipfile = "$dir\scoop.zip"
+Write-Output 'Downloading scoop...'
+dl $zipurl $zipfile
+
+Write-Output 'Extracting...'
+Add-Type -Assembly "System.IO.Compression.FileSystem"
+[IO.Compression.ZipFile]::ExtractToDirectory($zipfile, "$dir\_tmp")
+Copy-Item "$dir\_tmp\*master\*" $dir -Recurse -Force
+Remove-Item "$dir\_tmp", $zipfile -Recurse -Force
+
+Write-Output 'Creating shim...'
+shim "$dir\bin\scoop.ps1" $false
+
+# download main bucket
+$dir = "$scoopdir\buckets\main"
+$zipurl = 'https://github.com/ScoopInstaller/Main/archive/master.zip'
+$zipfile = "$dir\main-bucket.zip"
+Write-Output 'Downloading main bucket...'
+New-Item $dir -Type Directory -Force | Out-Null
+dl $zipurl $zipfile
+
+Write-Output 'Extracting...'
+[IO.Compression.ZipFile]::ExtractToDirectory($zipfile, "$dir\_tmp")
+Copy-Item "$dir\_tmp\*-master\*" $dir -Recurse -Force
+Remove-Item "$dir\_tmp", $zipfile -Recurse -Force
+
+ensure_robocopy_in_path
+ensure_scoop_in_path
+
+scoop config lastupdate ([System.DateTime]::Now.ToString('o'))
+success 'Scoop was installed successfully!'
+
+Write-Output "Type 'scoop help' for instructions."
+
+$erroractionpreference = $old_erroractionpreference # Reset $erroractionpreference to original value
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+
+#   Description:
+# This script will use Windows package manager to bootstrap Scoop and
+# install a list of packages. Script will also install Sysinternals Utilities
+# into your default drive's root directory.
+
+$packages = @(
+    "7zip"
+    "audacity"
+    "putty"
+    "python"
+    "qbittorrent"
+    "vlc"
+    "windirstat"
+    "wireshark"
+    "discord"
+    "dnspy"
+    "firefox-developer"
+    "ilspy"
+    "steam"
+    "teamviewer"
+    "geforce-experience"
+    "hxd"
+    "obs"
+    #"sysinternals"
+    "rufus"
+    #"dogtail.dotnet3.5sp1"
+    #"netfx-4.5.2-devpack"
+    #"dotnetcoresdk"
+    "dotnet"
+    "nirlauncher"
+    "bulk-crap-uninstaller"
+    #"jdk11"
+    #"jdk8"
+    "cutter"
+    "treesizefree"
+    "nvidia-geforce-experience"
+    "dotnet-sdk"
+    "ffmpeg"
+    "win32-disk-imager"
+
+)
+
+iwr -useb get.scoop.sh | iex -ErrorAction SilentlyContinue
+
+scoop install git -g
+scoop install aria2 -g
+
+scoop bucket add extras
+scoop bucket add nonportable
+scoop bucket add nirsoft
+scoop bucket add games
+scoop bucket add chadrien https://github.com/chadrien/scoop.git
+scoop bucket add Ash258 https://github.com/Ash258/Scoop-Ash258.git
+scoop bucket add scoop-java https://github.com/ScoopInstaller/Java
+
+
+echo "Creating daily task to automatically upgrade Scoop packages"
+# adapted from https://blogs.technet.microsoft.com/heyscriptingguy/2013/11/23/using-scheduled-tasks-and-scheduled-jobs-in-powershell/
+$ScheduledJob = @{
+    Name = "Scoop Daily Upgrade"
+    ScriptBlock = {scoop update *}
+    Trigger = New-JobTrigger -Daily -at 7pm
+    ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork
+}
+Register-ScheduledJob @ScheduledJob
+
+$ScheduledJob = @{
+    Name = "Scoop Daily Upgrade"
+    ScriptBlock = {scoop update}
+    Trigger = New-JobTrigger -Daily -at 7pm
+    ScheduledJobOption = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy StopExisting -RequireNetwork
+}
+Register-ScheduledJob @ScheduledJob
+
+echo "Installing Packages"
+$packages | %{scoop install $_ -g}
+
+
+
+
+
+echo "Installing Sysinternals Utilities to C:\Sysinternals"
+$download_uri = "https://download.sysinternals.com/files/SysinternalsSuite.zip"
+$wc = new-object net.webclient
+$wc.DownloadFile($download_uri, "/SysinternalsSuite.zip")
+Add-Type -AssemblyName "system.io.compression.filesystem"
+[io.compression.zipfile]::ExtractToDirectory("/SysinternalsSuite.zip", "/Sysinternals")
+echo "Removing zipfile"
+rm "/SysinternalsSuite.zip"
+
+start-process "cmd.exe" "/k cd $PSScriptRoot/VC && install_all.bat"
+
+
+If ((Get-WmiObject -Class "Win32_OperatingSystem").Caption -like "*Server*") {
+	Install-WindowsFeature -Name "Hyper-V" -IncludeManagementTools -WarningAction SilentlyContinue | Out-Null
+} Else {
+	Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -NoRestart -WarningAction SilentlyContinue | Out-Null
+}
+
+
+
 Start-Process "Explorer.exe" -Wait
